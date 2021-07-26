ee1c2591a8a69ecd49eafb0ed507ed598cf4bcfe
Elit <elit8888@gmail.com>
2021-07-26 23:36:14 +0800

    feat: add some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,8 +9,20 @@ alias py='python'
 alias py3='python3'
 alias activate-venv='source venv/bin/activate'
 
-towin10() {
+if command apt > /dev/null; then
+  towin10() {
     # Use `grep -i "menuentry '" /boot/grub/grub.cfg` to list the menuentry
     sudo grub-reboot "Windows 10 (on /dev/sda1)"
     sudo reboot
-}
+  }
+
+  reset_sound() {
+    rm -f ~/.config/pulse/*
+    sudo killall pulseaudio
+  }
+
+  auu() {
+    sudo apt update
+    sudo apt -y upgrade
+  }
+fi
