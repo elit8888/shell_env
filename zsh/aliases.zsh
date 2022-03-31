5f38adf9c14e255fd2a298cba6095cb8edd8a898
Elit Zou <elit8888@gmail.com>
2022-03-31 14:04:34 +0800

    feat: add mdcd function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,7 +10,12 @@ alias py2='docker run -it --rm python:2'
 alias py3='python3'
 alias activate-venv='source venv/bin/activate'
 
+mdcd() {
+  mkdir -p $1 && cd $1
+}
+
 if command -v apt > /dev/null; then
+  # debian specific
   towin10() {
     # Use `grep -i "menuentry '" /boot/grub/grub.cfg` to list the menuentry
     sudo grub-reboot "Windows 10 (on /dev/sda1)"
